bedc3860d10f472467c69aabfdae880f1ff3178b
Yonga Springfield <springfieldyonga@outlook.com>
2021-03-09 10:51:49 +0100

    feat(index.svelte):finished animation

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Nav from '../components/Nav.svelte';
+
+	export let segment: string;
+</script>
+
+<style>
+	main {
+		height:100vh;
+		width:100vw;
+		padding:1em;
+		display:flex;
+		align-items:center;
+		justify-content:center;
+	}
+</style>
+
+<!-- <Nav {segment}/> -->
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Logo from './../icons/logo.svelte'
+</script>
+
+<style>
+	
+</style>
+
+<svelte:head>
+	<title>Reignever</title>
+</svelte:head>
+
+<Logo />
